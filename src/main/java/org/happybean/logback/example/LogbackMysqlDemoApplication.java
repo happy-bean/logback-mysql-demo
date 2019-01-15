d04e90f2fd670a8789f576cd4744f2fd6b1a0243
bean <18222997200@163.com>
2019-01-15 10:54:02 +0800

    Initial commit

---
+++ src/main/java/org/happybean/logback/example/LogbackMysqlDemoApplication.java
@@ -0,0 +1,14 @@
+package org.happybean.logback.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackMysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackMysqlDemoApplication.class, args);
+	}
+
+}
+
